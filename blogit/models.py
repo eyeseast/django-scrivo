17ed0517d2e67c7018dfca1ff716ae99765eb6f0
Chris Amico <eyeseast@gmail.com>
2011-08-01 01:28:11 -0400

    Models set

--- blogit/models.py
+++ blogit/models.py
@@ -1,13 +1,16 @@
 import datetime
 from django.contrib.auth.models import User
 from django.db import models
+from django.template.defaultfilters import slugify
 
 from model_utils import Choices
 from model_utils.fields import MonitorField, SplitField, StatusField
 from model_utils.managers import PassThroughManager
 from model_utils.models import TimeStampedModel
+from revisions.shortcuts import TrashableVersionedModel
+from taggit.managers import TaggableManager
 
-from bloggit.managers import PostQuerySet
+from blogit.managers import PostQuerySet
 
 class PostBase(TimeStampedModel):
     """
@@ -15,7 +18,7 @@ class PostBase(TimeStampedModel):
     
     This class defines basic fields and logic for a blog post,
     including author, title, status (whether it's published or not)
-    and content.
+    and content, plus timestamp fields.
     
     By default, it uses an InheritanceManager, allowing subclasses
     to be included in a queryset.
@@ -34,9 +37,11 @@ class PostBase(TimeStampedModel):
     author = models.ForeignKey(User)
     published = models.DateTimeField(blank=True, null=True)
     status = StatusField(default=STATUS.draft)
-    status_changed = MonitorField(monitor='status')
+    status_changed = MonitorField(monitor='status', editable=False)
     
     # content
+    title = models.CharField(max_length=255, blank=True)
+    slug = models.SlugField(max_length=255)
     excerpt = models.TextField(blank=True,
         help_text="Add a manual excerpt")
     body = SplitField(blank=True)
@@ -56,3 +61,19 @@ class PostBase(TimeStampedModel):
             self.published = datetime.datetime.now()
         self.status = self.STATUS.public
         self.save()
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title or self.id)
+        super(PostBase, self).save(*args, **kwargs)
+    
+
+class Post(PostBase, TrashableVersionedModel):
+    """
+    A basic blog post:
+     - tags provided by django-taggit
+     - versioning provided by django-revisions
+    """
+    allow_comments = models.BooleanField(default=True)
+    tags = TaggableManager(blank=True)
+
