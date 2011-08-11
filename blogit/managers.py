78e61e7da49f7fa8abb930948eea095f0a795541
Chris Amico <eyeseast@gmail.com>
2011-08-10 23:17:16 -0400

    Added bulk publish and recall methods

--- blogit/managers.py
+++ blogit/managers.py
@@ -4,3 +4,19 @@ class PostQuerySet(InheritanceQuerySet):
     
     def public(self):
         return self.filter(status=self.model.STATUS.public)
+    
+    def publish(self):
+        """
+        Bulk publish all posts in the current queryset.
+        Note that this will NOT send pre_ or post_save signals.
+        """
+        now = datetime.datetime.now()
+        count = self.filter(published__isnull=False).update(status=self.model.STATUS.public)
+        count += self.filter(published__isnull=True).update(status=self.model.STATUS.public, published=now)
+        return count
+
+    def recall(self):
+        """
+        Silently unpublish all posts in the current queryset
+        """
+        return self.update(status=self.model.STATUS.hidden)
