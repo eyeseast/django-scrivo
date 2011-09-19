b5d400d726096b2627eb9aac933e24777583fe5d
Chris Amico <eyeseast@gmail.com>
2011-09-18 23:58:39 -0400

    More view tests

--- scrivo/tests/views.py
+++ scrivo/tests/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 
 from scrivo.models import Post
-from scrivo.settings import INDEX_POST_COUNT
+from scrivo.settings import DEFAULT_PAGINATE_BY, INDEX_POST_COUNT
 from scrivo.tests.base import BlogPostTest, f
 
 class PostViewTest(BlogPostTest):
@@ -31,14 +31,42 @@ class PostViewTest(BlogPostTest):
         response = self.client.get(reverse('scrivo_archive_index'))
         self.assertEqual(response.status_code, 200)
         
-        latest = response.context.get('latest')
-        if not latest:
+        posts = response.context.get('object_list')
+        if not posts:
             self.fail("No posts in context")
         
-        self.assertEqual(latest.count(), INDEX_POST_COUNT)
+        self.assertEqual(posts.count(), INDEX_POST_COUNT)
                 
         
     def test_year_archive(self):
         response = self.client.get(reverse('scrivo_year_archive', args=[2011]))
         self.assertEqual(response.status_code, 200)
+        
+        posts = response.context.get('object_list')
+        if not posts:
+            self.fail("No posts in context")
+        
+        self.assertEqual(posts.count(), DEFAULT_PAGINATE_BY)
+    
+    def test_month_archive(self):
+        response = self.client.get(reverse('scrivo_month_archive', args=[2011, 'jan']))
+        self.assertEqual(response.status_code, 200)
+        
+        posts = response.context.get('object_list')
+        if not posts:
+            self.fail("No posts in context")
+        
+        self.assertEqual(len(posts), DEFAULT_PAGINATE_BY)
+    
+    def test_day_archive(self):
+        response = self.client.get(reverse('scrivo_day_archive', args=[2011, 'jan', 5]))
+        self.assertEqual(response.status_code, 200)
+        
+        posts = response.context.get('object_list')
+        if not posts:
+            self.fail("No posts in context")
+        
+        # since we're doing one post per day
+        self.assertEqual(len(posts), 1)
+        
         
